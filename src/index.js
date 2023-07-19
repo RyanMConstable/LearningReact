c2b3de6c43221f0ea97fda030e8ff11b07624ba9
ryanmconstable <ryanmconstable@gmail.com>
2023-07-19 15:52:54 -0400

    Added keys to each new component

--- src/index.js
+++ src/index.js
@@ -32,11 +32,15 @@ function Car(props){
 }
 
 function Garage(){
-  const cars = ["Ford", "BMW", "Audi"]
+  const cars = [
+    {id:1, brand:"Ford"}, 
+    {id:2, brand:"BMW"},
+    {id:3, brand:"Audi"}]
+
   return (
     <>
       <h1>What cars are in my garage?</h1>
-      <ul>{cars.map((car) => <Car brand={car}/>)}</ul>
+      <ul>{cars.map((car) => <Car key={car.id} brand={car.brand}/>)}</ul>
     </>
   )
 }
